b170350f92b1aa44f2d0859acd548b19e0213b94
quidcain <quidcain@gmail.com>
2019-09-20 00:35:45 +0300

    Add get-indentor-home

--- src/indentor/core.clj
+++ src/indentor/core.clj
@@ -1,6 +1,11 @@
 (ns indentor.core
   (:gen-class))
 
+(defn get-indentor-home
+  []
+  (or
+   (System/getenv "INDENTOR_HOME")
+   (throw (Exception. "INDENTOR_HOME is not configured"))))
 
 (defn -main
   [& args]
